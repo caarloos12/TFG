5da37c8873c8cfd587c8e9eee2962462da4cd1e6
caarloos12 <c.sanroman.cvg@gmail.com>
2025-06-30 19:40:16 +0200

    Update mortonTSP.py

--- mortonTSP.py
+++ mortonTSP.py
@@ -1,3 +1,12 @@
+"""
+==============================================================================
+|| TFG - Trabajo de Fin de Grado en Matemáticas de Carlos San Román Cazorla ||
+==============================================================================
+"""
+
+"Este código ha sido realizado por Carlos San Román Cazorla"
+"para el desarrollo de su Trabajo de Fin de Grado en Matemáticas."
+
 import numpy as np
 import re
 import pymorton # Importamos la librería
