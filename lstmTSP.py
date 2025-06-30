9fd6d1e2298773a23017735a4a413620c480db0f
caarloos12 <c.sanroman.cvg@gmail.com>
2025-06-30 19:39:31 +0200

    Update lstmTSP.py

--- lstmTSP.py
+++ lstmTSP.py
@@ -1,3 +1,12 @@
+"""
+==============================================================================
+|| TFG - Trabajo de Fin de Grado en Matemáticas de Carlos San Román Cazorla ||
+==============================================================================
+"""
+
+"Este código ha sido realizado por Carlos San Román Cazorla"
+"para el desarrollo de su Trabajo de Fin de Grado en Matemáticas."
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
